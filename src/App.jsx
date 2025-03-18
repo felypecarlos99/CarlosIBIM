3a0d25f9b03d93063cf3406b4356cf401315bb77
Felype Carlos <felype.carlosilva@gmail.com>
2025-03-18 16:07:59 -0300

    primeiro projeto site

---
+++ src/App.jsx
@@ -0,0 +1,148 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <header>
+        <img src="png-clipart-used-car-computer-icons-vehicle-car-icon-angle-rectangle-removebg-preview.png"
+            width="50px">
+        <p class="carshop">CAR SHOP</p>
+    </header>
+    <div class="container">
+        <img class="imgheader" src="peugeot.webp">
+        <p class="texto1">Os melhores</p>
+        <p class="texto2">carros</p>
+        <p class="texto3">do mundo</p>
+    </div>
+
+    <div class="containerzao">
+
+
+        <div class="container2">
+            <div class="blue">
+            <div class="gridItem red"></div>
+        </div>
+                <img class="imagem1" src="4.webp">
+                <img class="cifrao" src="https://images.vexels.com/content/266053/preview/dollar-sign-money-icon-941b0d.png">
+                <p class="textoimgs">PAGANI </p>
+                <p class="textoimgs2">ZONDA </p>
+                <p class="textoimgs3">20 milhões</p>
+        </div>
+
+
+
+        <div class="container2">
+            <div class="blue">
+            <div class="gridItem red"></div>
+        </div>
+                <img class="imagem1" src="5.jpg">
+                <img class="cifrao" src="https://images.vexels.com/content/266053/preview/dollar-sign-money-icon-941b0d.png">
+                <p class="textoimgs4">LA </p>
+                <p class="textoimgs2">VOITURE </p>
+                <p class="textoimgs3">20 milhões</p>
+        </div>
+
+
+
+
+
+        <div class="container2">
+            <div class="blue">
+            <div class="gridItem red"></div>
+        </div>
+                <img class="imagem1" src="1.webp">
+                <img class="cifrao" src="https://images.vexels.com/content/266053/preview/dollar-sign-money-icon-941b0d.png">
+                <p class="textoimgs5">777</p>
+                <p class="textoimgs6">HYPERCAR </p>
+                <p class="textoimgs3">20 milhões</p>
+        </div>
+
+
+        <div class="container2">
+            <div class="blue">
+            <div class="gridItem red"></div>
+        </div>
+            <img class="imagem1" src="sp-automotive-chaos-05-20211103214458-1280x852.jpg">
+                <img class="cifrao" src="https://images.vexels.com/content/266053/preview/dollar-sign-money-icon-941b0d.png">
+                <p class="textoimgs9">SP</p>
+                <p class="textoimgs10">AUTOMOTIVE</p>
+                <p class="textoimgs3">20 milhões</p>
+            </div>
+
+
+
+
+        <div class="container2">
+            <div class="blue">
+            <div class="gridItem red"></div>
+            </div>
+                <img class="imagem1" src="https://www.automaistv.com.br/wp-content/uploads/2020/11/2021-Mercedes-Maybach-S-Class-65_edited-990x556.jpg">
+                <img class="cifrao" src="https://images.vexels.com/content/266053/preview/dollar-sign-money-icon-941b0d.png">
+                <p class="textoimgs8">MERCEDES</p>
+                <p class="textoimgs2">MAYBACH</p>
+                <p class="textoimgs3">20 milhões</p>
+        </div>
+
+
+
+        <div class="container2">
+            <div class="blue">
+            <div class="gridItem red"></div>
+        </div>
+                <img class="imagem1" src="4ya3dvpxnl861.webp">
+                <img class="cifrao1" src="https://images.vexels.com/content/266053/preview/dollar-sign-money-icon-941b0d.png">
+                <p class="textoimgs7">CENTODIECE</p>
+                <p class="textoimgs3">20 milhões</p>
+        </div>
+
+
+        <div class="container2">
+            <div class="blue">
+            <div class="gridItem red"></div>
+        </div>
+        <img class="imagem1" src="3.webp">
+                <img class="cifrao" src="https://images.vexels.com/content/266053/preview/dollar-sign-money-icon-941b0d.png">
+                <p class="textoimgs9">ROLLS-</p>
+                <p class="textoimgs10">ROYCE</p>
+                <p class="textoimgs3">20 milhões</p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="rodape">
+        <img class="iconecarro" src="./iconecarro.png">
+        <p class="textorodape">Siga nossas redes sociais</p>
+        <div class="iconesrodape">
+            <img class="iconeinsta" src="icons8-instagram-50.png">
+            <img class="iconeface" src="icons8-facebook-50.png">
+            <img class="iconeyoutube" src="icons8-youtube-50.png">
+            <img class="iconeinsta2" src="icons8-instagram-50.png">
+        </div>
+        <p class="textorodape1">Car Enterprises</p>
+        <p class="textorodape2">Maranello, Itália, 78950</p>
+        <div class="porcima">
+            <p class="esgotamento">ESGOTADO</p>
+        </div>
+        <div class="circulo">
+            <p class="textocirculo">20% OFF</p>
+        </div>
+        <div class="circulo2">
+            <p class="textocirculo2">20% OFF</p>
+        </div>
+        <div class="circulo3">
+            <p class="textocirculo">20% OFF</p>
+    </div>
+        
+    </>
+  )
+}
+
+export default App
+
+
